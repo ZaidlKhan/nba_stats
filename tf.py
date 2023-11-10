004fb7198177f0cab93032c1976ed8540c01170e
Zaid Khan <zkhan1605@gmail.com>
2023-11-09 19:05:40 -0800

    added new dataset

--- tf.py
+++ tf.py
@@ -1,53 +1,12 @@
 import csv
 import pandas as pd
-import numpy as np
-import sklearn
-from sklearn import linear_model, preprocessing, ensemble
-from sklearn.neighbors import KNeighborsClassifier
-<<<<<<< HEAD
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.impute import SimpleImputer
-=======
->>>>>>> 1de9927886771824d50981148e795c6d7701a8be
+import csv
+import matplotlib as plot
 
-path = "player_stats/NBA_Player_Stats.csv"
+path = "nba_stats/player_stats/NBA_Player_Stats.csv"
 
-data = pd.read_csv(path)
-<<<<<<< HEAD
+data = pd.read_csv(path, sep=";")
 
-data = data[["Age", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA",
-             "2P", "2PA", "2P%", "eFG%", "FT", "FT%", "ORB", "DRB", "TRB", "AST",
-             "STL", "BLK", "TOV", "PF", "PTS"]]
+df = pd.DataFrame(data, {"Player": "Lebron James"})
 
-data.fillna(0, inplace=True)
-
-predict = "PTS"
-
-y = np.array(data[predict])
-x = np.array(data.drop(predict, axis=1))
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-    x, y, test_size=0.1)
-
-imputer = SimpleImputer(strategy="mean")
-imputer.fit(x_train)
-x_train = imputer.transform(x_train)
-x_test = imputer.transform(x_test)
-
-linear = linear_model.LinearRegression()
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-
-print(data.head())
-=======
-
-data = data[["Player", "Age", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA",
-             "2P", "2PA" , "2P%", "eFG%", "FT", "FT%", "ORB", "DRB", "TRB", "AST",
-             "STL", "BLK", "TOV", "PF", "PTS"]]
-
-data.fillna(0, inplace = True)
-
-lebron_james_stats_only = data[data['Player'] == 'LeBron James']
-
-print(lebron_james_stats_only)
->>>>>>> 1de9927886771824d50981148e795c6d7701a8be
+print(df.head())
