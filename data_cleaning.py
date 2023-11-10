9aca4542f2a07c45046acad2879961819f97e709
ZaidlKhan <110517508+ZaidlKhan@users.noreply.github.com>
2023-11-10 01:03:22 -0800

    implemented model

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,89 +1,25 @@
-import csv
 import pandas as pd
-import numpy as np
-import sklearn
-from sklearn import linear_model, preprocessing, ensemble
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.utils import shuffle
 
-path = "player_stats/RS_player_stats_2019.csv"
+def data_clean(file_path):
+    data = pd.read_csv(file_path, index_col=0)
+    data = data.sort_values("date")
+    data = data.reset_index(drop=True)
+    del data["mp.1"]
+    del data["mp_opp.1"]
+    del data["index_opp"]   
 
-def openfile(file_path):
-    with open(file_path, 'r') as file:
-        reader = csv.reader(file, delimiter=';')
-        data = list(reader)
-        return data
+    def add_target(team):
+        team["target"] = team["won"].shift(-1)
+        return team
 
+    data = data.groupby("team", group_keys=False).apply(add_target)
 
-def add_year_to_csv(file_path, year):
-    data = openfile(file_path)
+    data.loc[pd.isnull(data["target"]), 'target'] = 2
+    data["target"] = data["target"].astype(int, errors="ignore")
 
-    for row in data:
-        row.insert(0, year)
-
-    with open(file_path, 'w', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerows(data)
-
-
-def remove_first_col(file_path):
-    df = pd.read_csv(file_path, delimiter=';')
-    df = df.drop(df.columns[0], axis=1)
-    df.to_csv(file_path, index=False, sep=';')
-
-
-def replace_string(file_path, str1, str2):
-    with open(file_path, 'r') as file:
-        data = file.read()
-
-    data = data.replace(str1, str2)
-
-    with open(file_path, 'w') as file:
-        file.write(data)
-
-
-data = pd.read_csv(path, sep=";")
-
-# le = preprocessing.LabelEncoder()
-# year = le.fit_transform(data["YEAR"])
-# full_name = le.fit_transform(data["FULL NAME"])
-# team = le.fit_transform(data["TEAM"])
-# position = le.fit_transform(data["POS"])
-# age = le.fit_transform(data["AGE"])
-# gp = le.fit_transform(data["GP"])
-# mpg = le.fit_transform(data["MPG"])
-# min_percent = le.fit_transform(data["MIN%"])
-# usg_percent = le.fit_transform(data["USG%"])
-# to_percent = le.fit_transform(data["TO%"])
-# fta = le.fit_transform(data["FTA"])
-# ft_percent = le.fit_transform(data["FT%"])
-# two_pa = le.fit_transform(data["2PA"])
-# two_p_percent = le.fit_transform(data["2P%"])
-# three_pa = le.fit_transform(data["3PA"])
-# three_p_percent = le.fit_transform(data["3P%"])
-# efg_percent = le.fit_transform(data["eFG%"])
-# ts_percent = le.fit_transform(data["TS%"])
-# ppg = le.fit_transform(data["PPG"])
-# rpg = le.fit_transform(data["RPG"])
-# trb = le.fit_transform(data["TRB%"])
-# spg = le.fit_transform(data["SPG"])
-# bpg = le.fit_transform(data["BPG"])
-# topg = le.fit_transform(data["TOPG"])
-# vi = le.fit_transform(data["VI"])
-# ortg = le.fit_transform(data["ORTG"])
-# drtg = le.fit_transform(data["DRTG"])
-
-data = data[["AGE", "GP", "MPG", "MIN%", "USG%", "TO%", "FTA",
-             "FT%", "2PA", "2P%", "3PA", "3P%", "eFG%", "TS%", "PPG", "RPG", "TRB%", "APG", "AST%",
-             "SPG", "BPG", "TOPG", "VI", "ORTG", "DRTG"]]
-
-predict = "PPG"
-
-x = np.array(data.drop([predict]), axis=1)
-y = np.array(data[predict])
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
-
-linear = linear_model.LinearRegression()
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+    nulls = pd.isnull(data)
+    nulls = nulls.sum()
+    nulls = nulls[nulls > 0]
+    valid_columns = data.columns[~data.columns.isin(nulls.index)]
+    data = data[valid_columns].copy()
+    return data
---
+++ model.py
@@ -0,0 +1,78 @@
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.feature_selection import SequentialFeatureSelector
+from sklearn.linear_model import RidgeClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+import data_cleaning
+import pandas as pd
+
+data = data_cleaning.data_clean("nba_games.csv")
+
+rr = RidgeClassifier(alpha=1)
+split = TimeSeriesSplit(n_splits=3)
+sfs = SequentialFeatureSelector(rr, n_features_to_select=30, direction="forward", cv=split)
+
+removed_columns = ["season", "date", "won", "target", "team", "team_opp"]
+selected_columns = data.columns[~data.columns.isin(removed_columns)]
+scaler = MinMaxScaler()
+data[selected_columns] = scaler.fit_transform(data[selected_columns]) 
+
+sfs.fit(data[selected_columns], data["target"])
+
+predictions = list(selected_columns[sfs.get_support()])
+
+def backtest(data, model, predictors, start=2, step=1):
+    all_predictions = []
+    seasons = sorted(data["season"].unique())
+    for i in range(start, len(seasons), step):
+        season = seasons[i]
+        train = data[data["season"] < season]
+        test = data[data["season"] == season]
+        model.fit(train[predictions], train["target"])
+        preds = model.predict(test[predictors])
+        preds = pd.Series(preds, index=test.index)
+
+        combined = pd.concat([test["target"], preds], axis=1)
+        combined.columns = ["actual", "predictions"]
+        all_predictions.append(combined)
+    return pd.concat(all_predictions)
+
+prediction = backtest(data, rr, predictions)
+
+data.groupby("home").apply(lambda x: x[x["won"] == 1].shape[0] /x.shape[0])
+
+data_rolling = data[list(selected_columns) + ["won", "team", "season"]]
+
+# Performance in last 10 games
+def find_team_average(team):
+    rolling = team.rolling(10).mean()
+    return rolling
+
+data_rolling = data_rolling.groupby(["team", "season"], group_keys=False).apply(find_team_average)
+rolling_cols = [f"{col}_10" for col in data_rolling.columns]
+data_rolling.columns = rolling_cols
+
+data = pd.concat([data, data_rolling], axis=1)
+data = data.dropna()
+
+def shift_col(team, col_name):
+    next_col = team[col_name].shift(-1)
+    return next_col
+
+def add_col(data, col_name):
+    return data.groupby("team", group_keys=False).apply(lambda x: shift_col(x, col_name))
+
+data["home_next"] = add_col(data, "home")
+data["team_opp_next"] = add_col(data, "team_opp")
+data["date_next"] = add_col(data, "date")
+
+full = data.merge(data[rolling_cols + ["team_opp_next", "date_next", "team"]], 
+                  left_on=["team", "date_next"], right_on=["team_opp_next", "date_next"])
+
+removed_columns = list(full.columns[full.dtypes == "object"]) + removed_columns
+selected_columns = full.columns[~full.columns.isin(removed_columns)]
+sfs.fit(full[selected_columns], full["target"])
+
+predictors = list(selected_columns[sfs.get_support()])
+predictions1 = backtest(full, rr, predictors)
+print(accuracy_score(predictions1["actual"]. predictions1["predictions"]))
