7245b67c8ed47d49f632647ffe09aa003d4c6426
ZaidlKhan <110517508+ZaidlKhan@users.noreply.github.com>
2023-11-26 19:00:18 -0800

    changed to forest model

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.linear_model import RidgeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score
 import data_cleaning
@@ -8,9 +9,11 @@ import pandas as pd
 
 data = data_cleaning.data_clean("nba_games.csv")
 
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
 rr = RidgeClassifier(alpha=1)
 split = TimeSeriesSplit(n_splits=3)
-sfs = SequentialFeatureSelector(rr, n_features_to_select=30, direction="forward", cv=split)
+sfs = SequentialFeatureSelector(rr, n_features_to_select=30, direction="backward", cv=split)
 
 removed_columns = ["season", "date", "won", "target", "team", "team_opp"]
 selected_columns = data.columns[~data.columns.isin(removed_columns)]
@@ -23,12 +26,13 @@ predictions = list(selected_columns[sfs.get_support()])
 
 def backtest(data, model, predictors, start=2, step=1):
     all_predictions = []
+
     seasons = sorted(data["season"].unique())
     for i in range(start, len(seasons), step):
         season = seasons[i]
         train = data[data["season"] < season]
         test = data[data["season"] == season]
-        model.fit(train[predictions], train["target"])
+        model.fit(train[predictors], train["target"])
         preds = model.predict(test[predictors])
         preds = pd.Series(preds, index=test.index)
 
@@ -49,9 +53,8 @@ def find_team_average(team):
     return rolling
 
 data_rolling = data_rolling.groupby(["team", "season"], group_keys=False).apply(find_team_average)
-rolling_cols = [f"{col}_10" for col in data_rolling.columns]
+rolling_cols = [f"{col}_5" for col in data_rolling.columns]
 data_rolling.columns = rolling_cols
-
 data = pd.concat([data, data_rolling], axis=1)
 data = data.dropna()
 
@@ -74,5 +77,6 @@ selected_columns = full.columns[~full.columns.isin(removed_columns)]
 sfs.fit(full[selected_columns], full["target"])
 
 predictors = list(selected_columns[sfs.get_support()])
-predictions1 = backtest(full, rr, predictors)
-print(accuracy_score(predictions1["actual"]. predictions1["predictions"]))
+prediction1 = backtest(data, rf, predictions)
+
+print(accuracy_score(prediction1["actual"], prediction1["predictions"]))
